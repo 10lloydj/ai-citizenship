1e1e5753e1a2f6abf27f73dcbee2f89fb3cf8062
10lloydj <10lloydj@gmail.com>
2025-12-24 22:37:03 +0000

    Pre-landing page import. Very soft changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import { SessionProvider } from "next-auth/react";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://chat.vercel.ai"),
-  title: "Next.js Chatbot Template",
-  description: "Next.js chatbot template using the AI SDK.",
+  title: "AI Citizenship Eligibility Checker",
+  description: "AI Citizenship Eligibility Checker",
 };
 
 export const viewport = {
